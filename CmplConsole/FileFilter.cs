951e35f1873837e0b1ed2f1824128e39b9f2098c
alexrjj <alexsoares2517@gmail.com>
2018-03-20 23:17:43 -0300

    Reconhece vários prefixos de Sobs

--- CmplConsole/FileFilter.cs
+++ CmplConsole/FileFilter.cs
@@ -10,15 +10,34 @@ namespace CmplConsole
         static void Main(string[] args)
         {
             string folder = Path.GetDirectoryName(Application.ExecutablePath);
-
-            var files = Directory.GetFiles(folder, "*.*", SearchOption.AllDirectories)
+            var files = Directory.GetFiles(folder, "*.*", SearchOption.TopDirectoryOnly)
                 .Where(s => s.EndsWith(".pdf") || s.EndsWith(".PDF"));
             foreach (string file in files)
             {
-                var s1 = file.Split(new string[] { "_" }, StringSplitOptions.None);
-                var s2 = s1[0].Split('_');
-                Console.WriteLine(Path.GetFileNameWithoutExtension(String.Join("_", s2)));
+                string f = Path.GetFileNameWithoutExtension(file);
+                if (f.StartsWith("SG_REF"))
+                {
+                    var s2 = file.Split('_');
+                    Console.WriteLine(Path.GetFileNameWithoutExtension(String.Join("_", s2.Skip(1).Take(2))));
+                }
+                else if (f.StartsWith("SG_RRC"))
+                {
+                    var s2 = file.Split('_');
+                    Console.WriteLine(Path.GetFileNameWithoutExtension(String.Join("_", s2.Skip(1).Take(2)).Replace("_", "-")));
+                }
+                else if (f.StartsWith("SG_"))
+                {
+                    var s2 = file.Split('_');  
+                    Console.WriteLine(Path.GetFileNameWithoutExtension(String.Join("_", s2.Skip(1).Take(1))));
+                }
+                
+                else
+                {
+                    Console.WriteLine("File not found");
+                }
             }
+                
+                
             Console.ReadKey();
         }
     }
