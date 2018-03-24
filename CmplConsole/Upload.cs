0df7d66c56287cdfee94ad45fbfc25e92a3cbe25
alexrjj <alexsoares2517@gmail.com>
2018-03-24 12:09:04 -0300

    Anexa o arquivo de acordo com sua categoria.

--- CmplConsole/Upload.cs
+++ CmplConsole/Upload.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Excel = NetOffice.ExcelApi;
 using System.Windows.Forms;
 using OpenQA.Selenium;
@@ -13,6 +9,7 @@ using OpenQA.Selenium.Support.UI;
 
 namespace CmplConsole
 {
+    
     class Upload
     {
         static void Main(string[] args)
@@ -21,13 +18,14 @@ namespace CmplConsole
             string url = "http://gomnet.ampla.com/";
             string urlUpload = "http://gomnet.ampla.com/Upload.aspx?numsob=";
             string folder = Path.GetDirectoryName(Application.ExecutablePath);
+            string log = folder + @"\log.txt";
 
-            //Headless Mode
-            ChromeOptions options = new ChromeOptions();
-            options.AddUserProfilePreference("download.default_directory", folder);
-            options.AddUserProfilePreference("download.prompt_for_download", false);
-            //options.AddArgument("--headless");
-            options.AddArgument("--window-size= 1600x900");
+            // ----  Chrome Settings ---- //
+                ChromeOptions options = new ChromeOptions();
+                //options.AddArgument("--headless");
+                options.AddArgument("--window-size= 1600x900");
+                IWebDriver driver = new ChromeDriver(options);
+            // ----  Chrome Settings ---- //
 
             //Informações de credenciais para login no GOMNET
             string credenciais = @"C:\gomnet.xlsx";
@@ -37,8 +35,8 @@ namespace CmplConsole
             Excel.Worksheet worksheet = (Excel.Worksheet)workbook.ActiveSheet;
             string login = worksheet.Range("A1").Value.ToString();
             string senha = worksheet.Range("A2").Value.ToString();
-
-            // close excel and dispose reference
+            
+            // Close excel and dispose reference
             excel.Quit();
             excel.Dispose();
 
@@ -52,16 +50,13 @@ namespace CmplConsole
             usrname.SendKeys(login);
             usrpass.SendKeys(senha);
             driver.FindElement(By.Id("ImageButton_Login")).Click();
-
-            //Tira screenshot da página atual
-            //Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
-            //ss.SaveAsFile("filename.png", ScreenshotImageFormat.Tiff);
             
             var files = Directory.GetFiles(folder, "*.*", SearchOption.TopDirectoryOnly)
                 .Where(s => s.EndsWith(".pdf") || s.EndsWith(".PDF"));
             foreach (string file in files)
             {
                 string f = Path.GetFileNameWithoutExtension(file);
+                string ext = Path.GetExtension(file);
                 if (f.StartsWith("SG_QUAL_"))
                 {
                     var s2 = file.Split('_');
@@ -71,7 +66,8 @@ namespace CmplConsole
                     {
                         string result = s3.Remove(lastIndex, "_".Length).Insert(lastIndex, "-");
                         driver.Navigate().GoToUrl(urlUpload + result);
-                    } else
+                    }
+                    else
                     {
                         driver.Navigate().GoToUrl(urlUpload + String.Join("_", s2.Skip(1).Take(2)));
                     }
@@ -109,32 +105,109 @@ namespace CmplConsole
                     {
                         Console.WriteLine("{0} not found.", f);
                     }
-                } catch (NoSuchElementException)
+                }
+                catch (NoSuchElementException)
                 {
                     try //Verifica se o arquivo já foi anexado
                     {
                         var anexo = driver.FindElement(By.XPath("*//a[contains(text(), '" + f + "')]"));
                         if (anexo.Displayed)
                         {
-                            Console.WriteLine("{0} already attached. ");
+                            Console.WriteLine("{0} already attached. ", f);
                         }
-                    } catch (NoSuchElementException)
+                    }
+                    catch (NoSuchElementException)
                     {
                         if (f.Contains("FORM_FISC"))
                         {
-                            //var categoria =  driver.FindElement(By.Id("drpCategoria"));
-                            //SelectElement selecCat = new SelectElement();
-                            //
-                            //
-                            //
-                            //categoria.select_by_visible_text('ENCERRAMENTO')
-                            //documento = Select(driver.find_element_by_id('DropDownList1'))
-                            //documento.select_by_visible_text('FORMULARIO DE FISCALIZACAO DE OBRA')
+                            SelectElement selectCat = new SelectElement(driver.FindElement(By.Id("drpCategoria")));
+                            selectCat.SelectByText("ENCERRAMENTO");
+                            SelectElement selectDoc = new SelectElement(driver.FindElement(By.Id("DropDownList1")));
+                            selectDoc.SelectByText("FORMULARIO DE FISCALIZACAO DE OBRA");
+                        }
+                        else if (f.Contains("AS_BUILT"))
+                        {
+                            SelectElement selectCat = new SelectElement(driver.FindElement(By.Id("drpCategoria")));
+                            selectCat.SelectByText("ENCERRAMENTO");
+                            SelectElement selectDoc = new SelectElement(driver.FindElement(By.Id("DropDownList1")));
+                            selectDoc.SelectByText("AS BUILT");
+                        }
+                        else if (f.Contains("APOIO_TRANSITO"))
+                        {
+                            SelectElement selectCat = new SelectElement(driver.FindElement(By.Id("drpCategoria")));
+                            selectCat.SelectByText("PROJETO");
+                            SelectElement selectDoc = new SelectElement(driver.FindElement(By.Id("DropDownList1")));
+                            selectDoc.SelectByText("CARTAS/OFICIOS");
+                        }
+                        else if (f.Contains("CCR"))
+                        {
+                            SelectElement selectCat = new SelectElement(driver.FindElement(By.Id("drpCategoria")));
+                            selectCat.SelectByText("PROJETO");
+                            SelectElement selectDoc = new SelectElement(driver.FindElement(By.Id("DropDownList1")));
+                            selectDoc.SelectByText("CARTAS/OFICIOS");
+                        }
+                        else if (f.Contains("_SGD_"))
+                        {
+                            SelectElement selectCat = new SelectElement(driver.FindElement(By.Id("drpCategoria")));
+                            selectCat.SelectByText("EXECUCAO");
+                            SelectElement selectDoc = new SelectElement(driver.FindElement(By.Id("DropDownList1")));
+                            selectDoc.SelectByText("SGD/TET");
+                        }
+                        else if (f.Contains("CLIENTE_VITAL"))
+                        {
+                            SelectElement selectCat = new SelectElement(driver.FindElement(By.Id("drpCategoria")));
+                            selectCat.SelectByText("PROJETO");
+                            SelectElement selectDoc = new SelectElement(driver.FindElement(By.Id("DropDownList1")));
+                            selectDoc.SelectByText("CARTAS/OFICIOS");
+                        }
+                        else if (f.Contains("_LOCACAO_") || f.Contains("_APR_") || f.Contains("_DESENHO_"))
+                        {
+                            SelectElement selectCat = new SelectElement(driver.FindElement(By.Id("drpCategoria")));
+                            selectCat.SelectByText("EXECUCAO");
+                            SelectElement selectDoc = new SelectElement(driver.FindElement(By.Id("DropDownList1")));
+                            selectDoc.SelectByText("LOCACAO");
+                        }
+                        else
+                        {
+                            SelectElement selectCat = new SelectElement(driver.FindElement(By.Id("drpCategoria")));
+                            selectCat.SelectByText("EXECUCAO");
+                            SelectElement selectDoc = new SelectElement(driver.FindElement(By.Id("DropDownList1")));
+                            selectDoc.SelectByText("PONTO DE SERVICO");
+                        }
+                        try
+                        {
+                            // Anexa o arquivo e clica no botão "Enviar..."
+                            driver.FindElement(By.Id("fileUPArquivo")).SendKeys(folder + @"\" + f + ext);
+                            driver.FindElement(By.Id("Button_Anexar")).Click();
+                        }
+                        catch (System.InvalidOperationException)
+                        {
+                            Console.WriteLine("{0} not found in folder.", f);
+                            continue;
+                        }
+                        try
+                        {
+                            var status = driver.FindElement(By.XPath("*//a[contains(text(), '" + f + ext + "')]"));
+                            if (status.Displayed)
+                            {
+                                Console.WriteLine("File {0}  attached sucessfully.", f);
+                                Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
+                                ss.SaveAsFile(f + ".png", ScreenshotImageFormat.Tiff);
+                            }
+                        }
+                        catch (NoSuchElementException)
+                        {
+                            if (!File.Exists(log))
+                            {
+                                using (StreamWriter sw = File.AppendText(log))
+                                {
+                                    sw.WriteLine(f);
+                                }
+                            }
                         }
                     }
                 }
             }
-
             //driver.Close();
             Console.ReadKey();
         }
