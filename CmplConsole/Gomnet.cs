030b3992aab20a4e6ce3f3e7bf90752ef1fb2c45
alexrjj <alexsoares2517@gmail.com>
2018-03-28 20:19:26 -0300

    Instancia var Excel apenas ao ser requisitado. Evita app aberto consumindo memória. Adicionado outras variáveis.

--- CmplConsole/Gomnet.cs
+++ CmplConsole/Gomnet.cs
@@ -17,23 +17,31 @@ namespace CmplConsole
         public static string folder;
         public static string sobs;
         public static string logSobsStatus;
-        public static Excel.Application excel = new Excel.Application();
+        public static Excel.Application excel;
         public static string login;
         public static string senha;
+        public static string googleForm;
+        public static string urlUpload;
+        public static string logUpload;
 
         public static void Settings()
         {
             url = "http://gomnet.ampla.com/";
             urlConsulta = "http://gomnet.ampla.com/ConsultaObra.aspx";
+            urlUpload = "http://gomnet.ampla.com/Upload.aspx?numsob=";
+            googleForm = "https://docs.google.com/forms/d/e/1FAIpQLSdqi7NxRSzKM0M3-ZQn5Fpn6rKriKJnw_0EPnlD3iScx18yXg/viewform";
             folder = Path.GetDirectoryName(Application.ExecutablePath);
             sobs = folder + @"\sobs.txt";
-            logSobsStatus = folder + @"\Sobs&Status.txt";
+            logSobsStatus = folder + @"\LogSobs&Status.txt";
+            logUpload = folder + @"\LogUpload.txt";
+
         }
 
         public static void Login()
         {
             //Informações de credenciais para login no GOMNET
             string credenciais = @"C:\gomnet.xlsx";
+            Excel.Application excel = new Excel.Application();
             excel.DisplayAlerts = false;
             Excel.Workbook workbook = excel.Workbooks.Open(credenciais);
             Excel.Worksheet worksheet = (Excel.Worksheet)workbook.ActiveSheet;
