9a9f7fd31e81e532dc70564bbc7c523747bd0f84
alexrjj <alexsoares2517@gmail.com>
2018-03-29 19:06:49 -0300

    Adicionado classe para baixar SGD

---
+++ CmplConsole/DownloadSGD.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using OpenQA.Selenium;
+
+namespace CmplConsole
+{
+    class DownloadSGD
+    {
+        public static void SGD()
+        {
+            Gomnet.Settings();
+            Chrome.Initializer();
+            Gomnet.Login();
+
+            string month = DateTime.Now.ToString("MM");
+            string line;
+            StreamReader file = new StreamReader(Gomnet.InSobDWG);
+            while ((line = file.ReadLine()) != null)
+            {
+                Chrome.driver.Navigate().GoToUrl(Gomnet.urlUpload + line);
+                try
+                {
+                    try
+                    {
+                        var pdfs = Chrome.driver.FindElements(By.PartialLinkText("'-' " + month + " '-18'"));
+                        foreach(int x in pdfs){
+                            Chrome.driver.FindElements(By.PartialLinkText("'-' " + month + " '-18'"))[pdf].Click();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
