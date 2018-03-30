33343b99b88a3f06c86a4960c378c79c249097d9
alexrjj <alexsoares2517@gmail.com>
2018-03-29 23:12:09 -0300

    Junta a função de Download DWG e SGD em apenas uma classe.

--- CmplConsole/Download.cs
+++ CmplConsole/Download.cs
@@ -4,8 +4,44 @@ using OpenQA.Selenium;
 
 namespace CmplConsole
 {
-    class DownloadDWG
+    class Download
     {
+        public static void SGD()
+        {
+            Gomnet.Settings();
+            Chrome.Initializer();
+            Gomnet.Login();
+
+            //string month = DateTime.Now.ToString("MM");
+            Console.Write("Month Value: ");
+            string month = Console.ReadLine();
+            string line;
+            StreamReader file = new StreamReader(Gomnet.InSobSGD);
+            while ((line = file.ReadLine()) != null)
+            {
+                Chrome.driver.Navigate().GoToUrl(Gomnet.urlUpload + line);
+                try
+                {
+                    try
+                    {
+                        var pdfs = Chrome.driver.FindElements(By.PartialLinkText("-" + month + "-18")).Count;
+                        for (int pdf = 0; pdf < pdfs; pdf++)
+                        {
+                            Chrome.driver.FindElements(By.PartialLinkText("-" + month + "-18"))[pdf].Click();
+                            Gomnet.FechaJanela();
+                        }
+                    }
+                    catch (NoSuchElementException)
+                    {
+                    }
+                }
+                catch (NoSuchElementException)
+                {
+                }
+            }
+            Console.Write("End of execution.");
+        }
+
         public static void DWG()
         {
             Gomnet.Settings();
@@ -30,7 +66,7 @@ namespace CmplConsole
                         }
                     }
                     catch (NoSuchElementException)
-                    {   
+                    {
                     }
                     try
                     {
