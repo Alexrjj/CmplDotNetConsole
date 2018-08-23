2504703669519a7e6aeb44d0e10caf79b5787817
alexrjj <alexsoares2517@gmail.com>
2018-08-23 10:55:14 -0300

    Adicionado classe para extração de info das sobs

---
+++ CmplConsole/Extrair.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using OpenQA.Selenium;
+
+namespace CmplConsole
+{
+    class Extrair
+    {
+        public static void SobTrabalho()
+        {
+            Gomnet.Settings();
+            Chrome.Initializer();
+            Gomnet.Login();
+
+            Chrome.driver.Navigate().GoToUrl(Gomnet.urlConsulta);
+
+            string line;
+            StreamReader file = new StreamReader(Gomnet.InSobTrab);
+            while ((line = file.ReadLine()) != null)
+            {
+                Chrome.driver.FindElement(By.Id("ctl00_ContentPlaceHolder1_TextBox_NumSOB")).Clear();
+                var sob = Chrome.driver.FindElement(By.Id("ctl00_ContentPlaceHolder1_TextBox_NumSOB"));
+                sob.SendKeys(line);
+                Chrome.driver.FindElement(By.Id("ctl00_ContentPlaceHolder1_ImageButton_Enviar")).Click();
+
+                try
+                {
+                    var numSob = Chrome.driver.FindElement(By.XPath("//*[@id='ctl00_ContentPlaceHolder1_Gridview_GomNet1']/tbody/tr[2]/td[8][contains(text(), '" + line + "' )]"));
+                    if (numSob.Displayed)
+                    {
+                        var numSobArquivo = Chrome.driver.FindElement(By.XPath("//*[@id='ctl00_ContentPlaceHolder1_Gridview_GomNet1']/tbody/tr[2]/td[8]")).Text;
+                        var numTrabArquivo = Chrome.driver.FindElement(By.XPath("//*[@id='ctl00_ContentPlaceHolder1_Gridview_GomNet1']/tbody/tr[2]/td[4]")).Text;
+
+                        using (StreamWriter sw = File.AppendText(Gomnet.OutSobTrab))
+                        {
+                            sw.WriteLine(numSobArquivo + " " + numTrabArquivo);
+                        }
+
+                    }
+                }
+                catch (NoSuchElementException)
+                {
+                    Console.WriteLine(line + " not found.");
+                    continue;
+                }
+            }
+            // Fecha o chromedriver
+            Chrome.driver.Close();
+        }
+
+        public static void SobStatus()
+        {
+            Gomnet.Settings();
+            Chrome.Initializer();
+            Gomnet.Login();
+
+            Chrome.driver.Navigate().GoToUrl(Gomnet.urlConsulta);
+
+            string line;
+            StreamReader file = new StreamReader(Gomnet.InSobStatus);
+            while ((line = file.ReadLine()) != null)
+            {
+                Chrome.driver.FindElement(By.Id("ctl00_ContentPlaceHolder1_TextBox_NumSOB")).Clear();
+                var sob = Chrome.driver.FindElement(By.Id("ctl00_ContentPlaceHolder1_TextBox_NumSOB"));
+                sob.SendKeys(line);
+                Chrome.driver.FindElement(By.Id("ctl00_ContentPlaceHolder1_ImageButton_Enviar")).Click();
+
+                try
+                {
+                    var numSob = Chrome.driver.FindElement(By.XPath("//*[@id='ctl00_ContentPlaceHolder1_Gridview_GomNet1']/tbody/tr[2]/td[8][contains(text(), '" + line + "' )]"));
+                    if (numSob.Displayed)
+                    {
+                        var numSobArquivo = Chrome.driver.FindElement(By.XPath("//*[@id='ctl00_ContentPlaceHolder1_Gridview_GomNet1']/tbody/tr[2]/td[8]")).Text;
+                        var numStatusArquivo = Chrome.driver.FindElement(By.XPath("//*[@id='ctl00_ContentPlaceHolder1_Gridview_GomNet1']/tbody/tr[2]/td[3]")).Text;
+
+                        using (StreamWriter sw = File.AppendText(Gomnet.OutSobStatus))
+                        {
+                            sw.WriteLine(numSobArquivo + " " + numStatusArquivo);
+                        }
+
+                    }
+                }
+                catch (NoSuchElementException)
+                {
+                    Console.WriteLine(line + " not found.");
+                    continue;
+                }
+            }
+            // Fecha o chromedriver
+            Chrome.driver.Close();
+        }
+
+        public static void SobEnergizacao()
+        {
+            //TODO: Implementar verificação de sob energizada.
+        }
+    }
+}
