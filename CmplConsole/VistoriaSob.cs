15a64a5d0cf5d2438bed6300e07ba9019d1fcb9c
alexrjj <alexsoares2517@gmail.com>
2018-08-28 17:06:29 -0300

    Adicionado função para vinculação de supervisor

--- CmplConsole/VistoriaSob.cs
+++ CmplConsole/VistoriaSob.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
 namespace CmplConsole
 {
@@ -10,7 +13,43 @@ namespace CmplConsole
     {
         public static void VincSupervisor()
         {
-            // TODO: Implementar vinculação de supervisor
+            Gomnet.Settings();
+            Chrome.Initializer();
+            Gomnet.Login();
+
+            string line;
+            StreamReader file = new StreamReader(Gomnet.InSobSup);
+            while ((line = file.ReadLine()) != null)
+            {
+                Chrome.driver.Navigate().GoToUrl(Gomnet.urlVincSup);
+                Chrome.driver.FindElement(By.Id("ctl00_ContentPlaceHolder1_TextBox_SOB")).Clear();
+                var sob = Chrome.driver.FindElement(By.Id("ctl00_ContentPlaceHolder1_TextBox_SOB"));
+                sob.SendKeys(line);
+                Chrome.driver.FindElement(By.Id("ctl00_ContentPlaceHolder1_ImageButton_Pesquisar")).Click();
+
+                try
+                {
+                    var supervisor = Chrome.driver.FindElement(By.Id("ctl00_ContentPlaceHolder1_gridViewTarefas_ctl02_ddlSupervisor"));
+                    var opcao = new SelectElement(supervisor);
+                    opcao.SelectByText("MESSIAS JOSE DE FARIA");
+                    Chrome.driver.FindElement(By.Id("ctl00_ContentPlaceHolder1_Button_VincularSupervisor")).Click();
+                    Chrome.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+                    try
+                    {
+                        Chrome.driver.SwitchTo().Alert().Accept();
+                        Console.WriteLine(line + "linked supervisor.");
+                    }
+                    catch (NoAlertPresentException)
+                    {
+                        continue;
+                    }
+                } catch (NoSuchElementException)
+                {
+                    Console.WriteLine(line + " not found.");
+                    continue;
+                }
+            }
+
         }
 
         public static void Vistoria()
