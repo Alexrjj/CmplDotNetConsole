bf6a0f5d291c5d1037333c8c6fda14f2cb10509e
alexrjj <alexsoares2517@gmail.com>
2018-08-28 14:59:49 -0300

    Adicionada classe para realização de vistoria da sob

---
+++ CmplConsole/VistoriaSob.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CmplConsole
+{
+    class VistoriaSob
+    {
+        public static void VincSupervisor()
+        {
+            // TODO: Implementar vinculação de supervisor
+        }
+
+        public static void Vistoria()
+        {
+            // TODO: Implementar vistoria de sob
+            // Obs.: Verificar se a sob já foi vistoriada e informar no console
+        }
+    }
+}
